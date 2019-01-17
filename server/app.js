920cac2a33805699e7c78ea86c9e17dcb3c4b325
valentineezeh <val.ezeh15@gmail.com>
2019-01-17 13:14:09 +0100

    chore: set up and configure base app - set up and configure express - set up database - set up eslint file - set up babelrc file

---
+++ server/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+// import router from './routes/index';
+
+// Instance of the express app
+const app = express();
+
+// Request logger
+app.use(morgan('dev'));
+
+// Parsing body data
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Connect all routes to application
+// app.use('/api/v1', router);
+
+app.get('/', (req, res) => {
+  res.status(200).send({
+    message: 'Welcome to Bank rating application..',
+  });
+});
+
+
+const port = process.env.PORT || 7000;
+
+app.listen(port);
+
+console.log(`Find me on http://localhost:${port}`);
+
+export default app;
